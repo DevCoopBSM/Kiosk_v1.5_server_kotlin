ecc028796ed99cd529eb0ab3e2524518645fb469
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-03-22 13:25:14 +0900

    FEAT: 엔티티 매핑

---
+++ src/main/kotlin/bsm/kiosk/kiosk_v2/domain/paylog/Paylog.kt
@@ -0,0 +1,38 @@
+package bsm.kiosk.kiosk_v2.domain.paylog
+
+import bsm.kiosk.kiosk_v2.domain.user.User
+import com.fasterxml.jackson.core.sym.Name
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class Paylog private constructor(
+  codeNumber: String,
+  date: LocalDateTime,
+  type: Short,
+  innerPoint: Int,
+  chargerId: String,
+  verifyKey: String,
+  studentName: User
+){
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "pay_num", nullable = false)
+  val payNum: Int = 0
+  var codeNumber: String = codeNumber
+    private set
+  var date: LocalDateTime = date
+    private set
+  var type: Short = type
+    private set
+  var innerPoint: Int = innerPoint
+    private set
+  var chargerId: String = chargerId
+    private set
+  var verifyKey: String = verifyKey
+    private set
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "studentNumber")
+  var studentName: User = studentName
+    private set
+}
